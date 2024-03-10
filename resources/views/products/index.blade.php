d5becc41d3f7d962a6e095a7f4380d7eb3445201
adriennecarlocortez <cortez.adrienne00@gmail.com>
2024-03-10 18:44:48 +0800

    spacing

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,7 +20,7 @@
       </a>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           {{-- Create Product Modal Toggle --}}
-          <button type="button" data-modal-target="create-product-modal" data-modal-toggle="create-product-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Product</button>
+          <button type="button" data-modal-target="create-product-modal" data-modal-toggle="create-product-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create a Product</button>
           {{-- Create Product Modal --}}
           @include('products.create')
       </div>
