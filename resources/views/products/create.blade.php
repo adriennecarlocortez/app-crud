7fe275b3be890d6a9a6a02a126fc196b3f69179e
adriennecarlocortez <cortez.adrienne00@gmail.com>
2024-03-11 12:03:47 +0800

    installed laravel breeze auth

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,12 @@
 <div id="create-product-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
   <div class="relative p-4 w-full max-w-2xl max-h-full">
     <!-- Modal content -->
-    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+    <div class="relative bg-white rounded-lg shadow">
+      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+        <h3 class="text-xl font-semibold text-gray-900">
             Create a New Product
         </h3>
-        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="create-product-modal">
+        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="create-product-modal">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
@@ -19,24 +19,24 @@
           @csrf
           @method('post')
           <div class="mb-5">
-            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
-            <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. Apple, Mango" required />
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Product Name</label>
+            <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="e.g. Apple, Mango" required />
           </div>
           <div class="mb-5">
-            <label for="qty" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
-            <input type="text" id="qty" name="qty" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. 100" required />
+            <label for="qty" class="block mb-2 text-sm font-medium text-gray-900">Quantity</label>
+            <input type="text" id="qty" name="qty" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="e.g. 100" required />
           </div>
           <div class="mb-5">
-            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-            <input type="text" id="price" name="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. 215.50" required />
+            <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Price</label>
+            <input type="text" id="price" name="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="e.g. 215.50" required />
           </div>
           <div class="mb-5">
-            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-            <input type="text" id="description" name="description" name="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. Green" required />
+            <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
+            <input type="text" id="description" name="description" name="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="e.g. Green" required />
           </div>
           <!-- Modal Buttons -->
-          <button  type="submit" data-modal-hide="create-product-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
-          <button data-modal-hide="create-product-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancel</button>
+          <button  type="submit" data-modal-hide="create-product-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create</button>
+          <button data-modal-hide="create-product-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancel</button>
         </form>
       </div>
     </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,25 +12,26 @@
   @include('products.alert')
 
   {{-- Header Nav --}}
-  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <nav class="bg-white border-gray-200">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="{{route('product.index')}}" class="flex items-center space-x-3 rtl:space-x-reverse">
             {{-- <img src="/images/svg/product.svg" class="h-8" alt="Product Logo" /> --}}
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Product</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap">Product</span>
       </a>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           {{-- Create Product Modal Toggle --}}
-          <button type="button" data-modal-target="create-product-modal" data-modal-toggle="create-product-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create a Product</button>
-          {{-- Create Product Modal --}}
-          @include('products.create')
+          <button type="button" data-modal-target="create-product-modal" data-modal-toggle="create-product-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Create a Product</button>
+         
       </div>
     </div>
   </nav>
+  {{-- Create Product Modal --}}
+  @include('products.create')
   
   {{-- Table --}}
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="border-b border-neutral-200 bg-[#332D2D] font-medium text-white dark:border-white/10">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead class="border-b border-neutral-200 bg-[#332D2D] font-medium text-white">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     ID
@@ -54,9 +55,9 @@
         </thead>
         <tbody>
             @foreach($products as $product)
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <tr class="bg-white border-b hover:bg-gray-50">
                   
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                       {{$product->id}}
                   </th>
                   <td class="px-6 py-4">
@@ -73,12 +74,12 @@
                   </td>
                   <td class="flex items-center px-6 py-4">
                       {{-- Edit Product Toggle --}}
-                      <a data-modal-target="edit-modal-{{$product->id}}" data-modal-toggle="edit-modal-{{$product->id}}"class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      <a data-modal-target="edit-modal-{{$product->id}}" data-modal-toggle="edit-modal-{{$product->id}}"class="font-medium text-blue-600 hover:underline">Edit</a>
                       {{-- Edit Product Modal--}}
                       @include('products.edit')
                       {{-- Remove Product Toggle --}}
                       <button type="menu" data-modal-target="remove-modal-{{$product->id}}" data-modal-toggle="remove-modal-{{$product->id}}">
-                        <a class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</a>
+                        <a class="font-medium text-red-600 hover:underline ms-3">Remove</a>
                       </button>
                       {{-- Remove Product Modal --}}
                       @include('products.remove')
